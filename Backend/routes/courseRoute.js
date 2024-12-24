32c5713daa38be92ee94921854b7d450442972fb
sarrthak008 <sarthaknavale08@gmail.com>
2024-12-24 12:40:30 +0530

    add new controller

--- Backend/routes/courseRoute.js
+++ Backend/routes/courseRoute.js
@@ -5,7 +5,7 @@ const courseRouter = Router()
 import {verifyJwtUser,verifyJWTOwner} from "../middlewares/jwtVarify.js"
 
 //controllers related courses...
-import  {getcourses,postcourse} from "../controllers/courseControl.js"
+import  {getcourses,postcourse,getmycourse} from "../controllers/courseControl.js"
 
 courseRouter.get('/getcourse',getcourses);
 courseRouter.post('/addcourse',verifyJWTOwner,postcourse)
