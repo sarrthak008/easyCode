a466c7875b6deb7a20a2a4ebfddbaf121b502c91
sarrthak008 <sarthaknavale08@gmail.com>
2024-12-25 19:05:56 +0530

    add footer

--- Frontend/src/view/Home.jsx
+++ Frontend/src/view/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import { Hero } from '../components/Hero'
 import Shadow from '../components/Shadow'
+import { Footer } from '../components/Footer'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
        <Navbar></Navbar>
        <Hero></Hero>
        <Shadow></Shadow>
+       <Footer></Footer>
     </div>
   )
 }
