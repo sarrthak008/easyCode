e132dbebbcd11505ed4882deda1bf0fa4c61eb8a
sarrthak008 <sarthaknavale08@gmail.com>
2024-12-25 15:57:57 +0530

    add nav bar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import CodeOffIcon from '@mui/icons-material/CodeOff';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="w-full h-16 bg-opacity-10 bg-gray-300">
+       <div className="flex justify-between items-center h-full px-4">
+        <div className="text-2xl font-bold text-white ">easyCode <CodeOffIcon /></div>
+       </div>
+    </div>
   )
 }
 
