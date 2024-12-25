6221615038ba9497a4354348d4dd749f261cd53a
sarrthak008 <sarthaknavale08@gmail.com>
2024-12-25 17:10:10 +0530

    hide scrolbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -5,7 +5,12 @@ const Navbar = () => {
   return (
     <div className="w-full h-16 bg-opacity-10 bg-gray-300">
        <div className="flex justify-between items-center h-full px-4">
-        <div className="text-2xl font-bold text-white ">easyCode <CodeOffIcon /></div>
+         <div className="text-2xl font-bold text-white ">easyCode <CodeOffIcon /></div>
+         <div className='w-1/3 flex justify-evenly items-center text-white text-[17px] font-semibold'>
+            <span className='cursor-pointer hover:text-green-400'>Home</span>
+            <span className='cursor-pointer hover:text-green-400'>Courses</span>
+            <button className="border py-[3px] px-10 rounded-xl text-green hover:bg-green-400 hover:text-white  shadow-md shadow-gray-700 hover:shadow-2xl hover:shadow-green-400 transition-all">Sign In</button>           
+         </div>
        </div>
     </div>
   )
