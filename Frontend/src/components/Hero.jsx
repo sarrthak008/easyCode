84ecd52f00ef8b0990cceb4dd9828e311ce6b7a5
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2024-12-25 17:43:11 +0530

    add hero contant

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -2,6 +2,62 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <div className='h-96 text-white w-full flex justify-center items-center flex-col relative '>
+
+
+        <h1 className=' text-6xl MyCustomFont uppercase font-bold'>Transform <span className='text-green'>Ideas</span> into <span className='text-green'>Code</span> !</h1>
+        <div className=' w-52 absolute bottom-0 right-40 text-center opacity-75 font-thin leading-5  '><p className=' mt-4'>Dive right into <span className='text-green font-bold'>coding</span> with our easy-to-follow live tutorials and tools <span className='text-green font-bold'>designed</span> for beginners and pros alike. Start building your first <span className='text-green font-bold'>project</span> today!</p></div>
+      </div>
+
+      <div className='   mt-36'>
+
+        <h1 className='text-center text-white font-bold text-3xl'>Explore Skill</h1>
+        <div className='flex justify-center items-center h-20 w-[75%] m-auto relative'>
+          <marquee scrollamount='2' behavior="alternate" className='text-green-400 opacity-50 font-bold text-4xl'>
+            <span className='m-5'>HTML</span>
+            <span className='m-5'>CSS</span>
+            <span className='m-5'>CSS</span>
+            <span className='m-5'>JAVASCRIP</span>
+            <span className='m-5'>REACT JS</span>
+            <span className='m-5'>NODE JS</span>
+            <span className='m-5'>EXPRESS JS</span>
+            <span className='m-5'>MONGODB</span>
+            <span className='m-5'>C</span>
+            <span className='m-5'>PYTHON</span>
+            <span className='m-5'>GIT & GITHUB </span>
+            <span className='m-5'>HTML</span>
+            <span className='m-5'>CSS</span>
+            <span className='m-5'>CSS</span>
+            <span className='m-5'>JAVASCRIP</span>
+            <span className='m-5'>REACT JS</span>
+            <span className='m-5'>NODE JS</span>
+            <span className='m-5'>EXPRESS JS</span>
+            <span className='m-5'>MONGODB</span>
+            <span className='m-5'>C</span>
+            <span className='m-5'>PYTHON</span>
+            <span className='m-5'>GIT & GITHUB </span>
+            <span className='m-5'>HTML</span>
+            <span className='m-5'>CSS</span>
+            <span className='m-5'>CSS</span>
+            <span className='m-5'>JAVASCRIP</span>
+            <span className='m-5'>REACT JS</span>
+            <span className='m-5'>NODE JS</span>
+            <span className='m-5'>EXPRESS JS</span>
+            <span className='m-5'>MONGODB</span>
+            <span className='m-5'>C</span>
+            <span className='m-5'>PYTHON</span>
+            <span className='m-5'>GIT & GITHUB </span>
+
+          </marquee>
+          <div className=' bg-gradient-to-l to-[#060A23] from-transparent w-[50%] absolute top-0 left-0  h-full'> </div>
+          <div className=' bg-gradient-to-r to-[#060A23] from-transparent w-[50%] absolute top-0 right-0  h-full'></div>
+
+        </div>
+      </div>
+    </>
+
+
   )
 }
+
