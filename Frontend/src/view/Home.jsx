8cde9838685da227805aa8fbb19576d54784fc51
sarrthak008 <sarthaknavale08@gmail.com>
2024-12-27 12:20:15 +0530

    add server url

--- Frontend/src/view/Home.jsx
+++ Frontend/src/view/Home.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import { Hero } from '../components/Hero'
 import Shadow from '../components/Shadow'
 import { Footer } from '../components/Footer'
+import axios from 'axios'
+import { use } from 'react'
+import { useSnackbar } from 'notistack'
+const API_URl = import.meta.env.VITE_SERVER_URI
 
 const Home = () => {
+
+  
+    const { enqueueSnackbar } = useSnackbar()
+
+   const loadserver = async () =>{
+      try {
+        const responce = await axios.get(`${API_URl}/connect`)
+         if(responce.data.success){
+            enqueueSnackbar(`${responce.data.message}`, { variant: "success" })
+         }
+      } catch (error) {
+         enqueueSnackbar(`${error.message}`, { variant: "success" })
+      }
+   }
+
+   useEffect(()=>{
+    loadserver()
+   },[]);
+  
+
   return (
     <div className='w-lvw min-h-lvh bg-dark'>
        <Navbar></Navbar>
