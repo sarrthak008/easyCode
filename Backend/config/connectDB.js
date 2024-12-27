91fa322493b9ac735342c3e972819a4089f23273
sarrthak008 <sarthaknavale08@gmail.com>
2024-12-28 04:04:40 +0530

    add new mongo db

--- Backend/config/connectDB.js
+++ Backend/config/connectDB.js
@@ -1,18 +1,17 @@
-import mongoose from 'mongoose';
+import e from "cors";
+import mongoose from "mongoose";
 
 const connectToDb = async () => {
-  try {
-    const connection = await mongoose.connect(process.env.MONGO_CONNECT_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    if (connection) {
-      console.log('Connected to database...');
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_CONNECT_URI);
+        if (!connection) {
+            console.log('connecting error...')
+        } else {
+            console.log('connected to database...')
+        }
+    } catch (error) {
+        console.log(error)
     }
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-  }
-};
+}
 
-export default connectToDb;
+export default connectToDb
