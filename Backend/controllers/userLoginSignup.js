026ad042181a3eae563111f6f9770edcb3378096
sarrthak008 <sarthaknavale08@gmail.com>
2025-01-09 10:08:05 +0530

    add new settign

--- Backend/controllers/userLoginSignup.js
+++ Backend/controllers/userLoginSignup.js
@@ -164,7 +164,7 @@ const postLogin = async (req, res) => {
 
          res.cookie("token",token,{
             sameSite: 'None',
-            secure: true,
+            secure: false,
             maxAge: 604800000,
             path:'/'
          });
