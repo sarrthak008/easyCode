284e23f63426eeff81ff21968496f09bff9f8403
sarrthak008 <sarthaknavale08@gmail.com>
2025-01-11 19:49:42 +0530

    add new tag

--- Frontend/src/view/Dashboard.jsx
+++ Frontend/src/view/Dashboard.jsx
@@ -3,8 +3,9 @@ import Navbar from '../components/Navbar'
 import axios from 'axios'
 import { closeSnackbar, useSnackbar } from 'notistack'
 import Appbtn from '../components/Appbtn'
+import { Link } from 'react-router-dom'
 const API_URL = import.meta.env.VITE_SERVER_URI
-
+import Shadow from "../components/Shadow"
 
 const CourseCard = ({course}) => {
   return (
@@ -39,12 +40,14 @@ const Dashboard = () => {
   const [mycourses, setMycourses] = useState([])
   const { enqueueSnackbar } = useSnackbar()
   const [openFeedback ,setOpenFeedback] = useState(false)
+  const [feedback,setFeedBack] = useState('')
 
   const loadMycourses = async () => {
+   
     let snackId = enqueueSnackbar('loding your courses data..', { variant: "info" })
     try {
       const responce = await axios.post(`${API_URL}/api/course/getmycourse`, null, { withCredentials: true })
-      console.log(responce?.data?.data?.courses)
+      //console.log(responce?.data?.data?.courses)
       if (responce.data.success) {
         setMycourses(responce?.data?.data?.courses)
         enqueueSnackbar(`course loaded`, { variant: 'success' })
@@ -61,8 +64,34 @@ const Dashboard = () => {
     }
   }
 
+  const postFeedBack = async ()=>{
+      try {
+         let snackId = enqueueSnackbar('submiting your feedback', { variant: "info" })
+         const responce = await axios.post(`${API_URL}/api/feedback/addfeedback`,{description:`${feedback}`},{withCredentials:true});
+          if(responce.data.success){
+            closeSnackbar(snackId)
+            loadPreviousFeedBack()
+            return enqueueSnackbar(`${responce.data.message}`, { variant: "success" })
+          }
+        return enqueueSnackbar(`${responce.data.message}`, { variant: 'warning' })
+      } catch (error) {
+         closeSnackbar(snackId)
+         return enqueueSnackbar(`${error.message}`, { variant: "error" })
+      }
+  }
+
+  const loadPreviousFeedBack = async () => {
+     try {
+        const responce = await axios.get(`${API_URL}/api/feedback/getfeedback`,{withCredentials:true})
+        setFeedBack(responce.data?.data?.description)
+     } catch (error) {
+       console.log(error)
+     }
+  }
+
   useEffect(() => {
     loadMycourses()
+    loadPreviousFeedBack()
   }, [])
 
   return (
@@ -71,6 +100,11 @@ const Dashboard = () => {
     <div className='h-screen w-screen relative'>
       <div className='h-screen w-screen flex items-center justify-center flex-wrap mt-24'>
         {
+          mycourses?.length == 0 ? <div className=' h-80 font-medium text-7xl text-linear text-center'>
+             <div>Nothing To Show</div>
+             <Link>
+             <Link to={'/course'}><span className='text-xl underline font-extralight text-gray-400'>buy course</span></Link></Link>
+            </div>  :
           mycourses?.map((course) => (
                 <CourseCard course={course}/>
               ))
@@ -82,11 +116,12 @@ const Dashboard = () => {
     {openFeedback ? <div className='h-full w-full absolute top-0 left-0 backdrop-blur-sm flex justify-center items-center '>
        <div className='h-[80%] w-[90%] sm:w-[70%] bg-gray-700 rounded-md p-3 mt-10'>
          <h1 className='text-white text-center text-5xl hevy'>add your feedback</h1>
-         <textarea  className=" bg-slate-500  p-2 w-[80%] mx-auto block text-white text-xl mt-8 resize-none outline-none border-0 border-b-2 border-b-green-400 h-[50%]"/>
-         <div className='w-[100%] text-white ml-[10%] mt-7'><Appbtn title='submit'/></div>
+         <textarea  value={feedback} onChange={(e)=>{setFeedBack(e.target.value)}} placeholder='enter your feedback related course.... '  className=" bg-slate-500  p-2 w-[80%] mx-auto block text-white text-xl mt-8 resize-none outline-none border-0 border-b-2 border-b-green-400 h-[50%]"/>
+         <div className='w-[100%] text-white ml-[10%] mt-7'><Appbtn title='submit' onClick={()=>postFeedBack()}/></div>
        </div>
        <span  className='block text-white relative -top-56 text-4xl hover:text-green-400 cursor-pointer '><i className="ri-close-line" onClick={()=>{setOpenFeedback(false)}}></i></span>
     </div> : null}
+   <Shadow/>
     </>
   )
 }
