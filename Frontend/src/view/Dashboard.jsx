0a63e1b8c4ddbcd09afc2a837608d9357d468ef2
sarrthak008 <sarthaknavale08@gmail.com>
2025-01-13 18:00:57 +0530

    adduser speed dialder

--- Frontend/src/view/Dashboard.jsx
+++ Frontend/src/view/Dashboard.jsx
@@ -5,7 +5,6 @@ import { closeSnackbar, useSnackbar } from 'notistack'
 import { Link } from 'react-router-dom'
 const API_URL = import.meta.env.VITE_SERVER_URI
 import Shadow from "../components/Shadow"
-import UserSpeedDialer from '../components/userSpeedDialer'
 
 const CourseCard = ({ course }) => {
   return (
@@ -112,10 +111,10 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <Shadow />
+      {/* <Shadow />
       <div className='fixed bottom-20 right-14'>
         <UserSpeedDialer />
-      </div>
+      </div> */}
     </>
   )
 }
