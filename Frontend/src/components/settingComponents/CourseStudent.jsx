ebe3d3943ceea4baa3ad80973e0f5cc023677b3d
sarrthak008 <sarthaknavale08@gmail.com>
2025-01-20 11:08:25 +0530

    update user cards

--- Frontend/src/components/settingComponents/CourseStudent.jsx
+++ Frontend/src/components/settingComponents/CourseStudent.jsx
@@ -6,23 +6,23 @@ const API_URL = import.meta.env.VITE_SERVER_URI
 
 
 const StudentCards = ({user})=>{
-  console.log(user)
+  //console.log(user)
   return(
     <div className=' w-80 sm:w-96 bg-gray-900 backdrop-blur-md p-4 ml-2 sm:ml-0'>
       <div className='flex text-gray-400 gap-4 '>
-         <div className='h-[65px] w-[65px] bg-red-300 rounded-full overflow-hidden'>
-           <img src={user?.profilePic}/>
+         <div className='h-[50px] w-[50px] m-1 sm:h-[65px] sm:w-[65px] rounded-full overflow-hidden'>
+           <img src={user?.profilePic} className='object-cover'/>
          </div>
-         <div>
-           <span className='text-[16px] line-clamp-1'>{user?.mobile}</span>
-           <span className='text-[16px] line-clamp-1'>{user?.email}</span>
-           <span className='text-[16px] line-clamp-1'>{user?.name}</span>
+         <div className='overflow-hidden'>
+           <span className='text-[16px] line-clamp-1'><i className="ri-phone-fill"></i> {user?.mobile}</span>
+           <span className='text-[16px] block overflow-hidden whitespace-nowrap'><i className="ri-mail-fill"></i> {user?.email}</span>
+           <span className='text-[16px] line-clamp-1'><i className="ri-user-3-fill"></i> {user?.name}</span>
          </div>
       </div>
-      <div className='text-gray-400 mt-1'>requested at : {user?.createdAt?.split("T")[0]}</div>
+      <div className='text-gray-400 mt-1'>info loading...</div>
       <div className='flex-wrap flex mt-2 '>
-         <button className='w-full sm:w-[40%] m-3 text-center shadow-lg hover:bg-green-500   bg-green-400 py-1 text-2xl'>accept <i className="ri-user-add-line"></i></button>
-         <button className='w-full sm:w-[40%] m-3 text-center shadow-lg hover:bg-red-500  bg-red-800  py-1 text-2xl' >reject <i className="ri-user-unfollow-line"></i></button>
+         <button className='w-full sm:w-[40%] m-3  rounded-sm text-center shadow-lg hover:bg-green-500   bg-green-400 py-1 text-2xl'>accept <i className="ri-user-add-line"></i></button>
+         <button className='w-full sm:w-[40%] m-3  rounded-sm text-center shadow-lg hover:bg-red-500  bg-red-800  py-1 text-2xl' >reject <i className="ri-user-unfollow-line"></i></button>
       </div>
     </div>
   )
