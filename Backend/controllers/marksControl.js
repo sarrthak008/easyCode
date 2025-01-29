f3c1f7d92ddcfda5b03bd8de694343c0c0d137eb
sarrthak008 <sarthaknavale08@gmail.com>
2025-01-29 16:12:00 +0530

    add new api

--- Backend/controllers/marksControl.js
+++ Backend/controllers/marksControl.js
@@ -4,6 +4,16 @@ import { responder } from "../utils/responder.js";
 const postMarks = async (req, res) => { 
     try {
         const { marks, studentId, quizId } = req.body;
+
+        if (!marks || !studentId || !quizId) {
+            return responder(res, false, 'Marks, studentId and quizId are required', null, 400);
+        }
+
+        const marksExists = await Marks.findOne({ studentId, quizId });
+        if (marksExists) {
+            return responder(res, false, 'Marks already exists', null, 400);
+        }   
+
         const newMarks = new Marks({ marks, studentId, quizId });
         await newMarks.save();
         return responder(res, true, 'Marks added successfully', newMarks, 200);
@@ -12,6 +22,40 @@ const postMarks = async (req, res) => {
     }
 }
 
+const getMarks = async (req, res) => {
+    try {
+        const { studentId , quizId } = req.query;
+        if (!studentId) {
+            return responder(res, false, 'studentId is required', null, 400);
+        }
+
+        const marks = await Marks.findOne({ studentId, quizId });
+        if (!marks) {
+            return responder(res, false, 'Marks not found', null, 404);
+        }
+     return responder(res, true, 'Marks fetched successfully', marks, 200);
+    } catch (error) {
+        return responder(res, false, 'Failed to fetch marks', error.message, 500);
+    }
+}
+
+const getTopStudentofQuiz = async (req, res) => {
+    try {
+        const { quizId } = req.body;
+        if (!quizId) {
+            return responder(res, false, 'quizId is required', null, 400);
+        }
+
+        const topStudent = await Marks.find({ quizId }).sort({ marks: -1 }).limit(3);
+
+        if (!topStudent) {
+            return responder(res, false, 'Top student not found', null, 404);
+        }
+        return responder(res, true, 'Top student fetched successfully', topStudent, 200);
+    } catch (error) {
+        return responder(res, false, 'Failed to fetch top student', error.message, 500);
+    }
+}
 
 
-export { postMarks }
+export { postMarks ,getMarks,getTopStudentofQuiz}
--- Backend/routes/markRouter.js
+++ Backend/routes/markRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 const marksRouter  = Router()
 
-import {postMarks} from "../controllers/marksControl.js";
+import {postMarks,getMarks,getTopStudentofQuiz} from "../controllers/marksControl.js";
 
 marksRouter.post('/addmarks',postMarks);
+marksRouter.get('/getmarks',getMarks);
+marksRouter.get('/gettopstudent',getTopStudentofQuiz);
 
 
 export {marksRouter}
