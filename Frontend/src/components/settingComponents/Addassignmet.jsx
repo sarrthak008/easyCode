f476f7363c86017eac608ac3f91e556c40e0aef3
sarrthak008 <sarthaknavale08@gmail.com>
2025-03-08 10:56:00 +0530

    add new compnet to add assignment

--- Frontend/src/components/settingComponents/Addassignmet.jsx
+++ Frontend/src/components/settingComponents/Addassignmet.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Appinput from '../Appinput'
 
 const Addassignmet = () => {
   return (
     <>
             <div className='text-5xl text-white hevy text-green ml-4 pt-1 '>Add Assignment</div>
             <div className='h-screen w-screen flex items-center justify-center '>
-                 <div className='h-3/4 w-[90vw] sm:w-3/5 bg-gray-900 shadow-md shadow-gray-600'>
-
+                 <div className='h-3/4 w-[70vw] sm:w-3/5 bg-gray-900 shadow-md text-white  shadow-gray-800'>
+                    <Appinput title='enter assignmet title' />
                  </div>
             </div>
 
