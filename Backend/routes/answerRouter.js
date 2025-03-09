effdd31770b8058adb2947e8824dd006a473334b
sarrthak008 <sarthaknavale08@gmail.com>
2025-03-09 14:42:17 +0530

    update router

--- Backend/routes/answerRouter.js
+++ Backend/routes/answerRouter.js
@@ -9,7 +9,7 @@ import upload from "../middlewares/multer.js";
 import {verifyJwtUser} from "../middlewares/jwtVarify.js"
 
 // Routes
-answerRouter.post("/addassignment",upload.single("image"),verifyJwtUser, postassignment);
+answerRouter.post("/addassignment",upload.single("image"), postassignment);
 answerRouter.get("/getallanswers/:assignmentID", getallanswers);
 answerRouter.post("/postapprovalassignment/:_id",postapprovalassignment);
 answerRouter.get("/getuseranswer/:userId/:assignmentId",getUserAssignment);
