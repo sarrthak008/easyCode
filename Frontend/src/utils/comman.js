7ba9cef3c34503a072636181c5b409bb03aaa339
sarrthak008 <sarthaknavale08@gmail.com>
2025-06-29 17:24:56 +0300

    add url

--- Frontend/src/utils/comman.js
+++ Frontend/src/utils/comman.js
@@ -1,5 +1,5 @@
 const INFO ={
-     NEW_LAUNCH_WEB_URL :"",
+     NEW_LAUNCH_WEB_URL :"https://easycode-new-launch-xq6d.vercel.app/",
      BRAND_NAME : "team easycode",
      DEFAULT_MESSAGE : "good work !",
      PROFILE_URL : "https://res.cloudinary.com/dqjmbn0dy/image/upload/v1741770639/vite_knklng.svg"
